8c746b0140f3859792f9f9407ebbea981ee5b41e
kamleshc <kamlesh.designer@gmail.com>
2020-10-05 17:01:45 +0530

    async subscriptionTypes service.ts update

--- src/app/data/user-settings-data.service.ts
+++ src/app/data/user-settings-data.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { UserSettings } from './user-settings-interface';
@@ -6,11 +7,16 @@ import { UserSettings } from './user-settings-interface';
   providedIn: 'root'
 })
 export class UserSettingsDataService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getSubscriptionTypes(): Observable<string[]> {
+   // throw new Error('Method not implemented.');
+   return of (['Monthly', 'Annual', 'Lifetime']);
+  }
 
-  postUserSettingsForm(userSettings: UserSettings) : Observable<UserSettings>{
-    return of(userSettings);
+  postUserSettingsForm(userSettings: UserSettings) : Observable<any>{
+    //return of(userSettings);
+    return this.http.post('https://dd7440f431521c078cdf941670af0599.m.pipedream.net', userSettings);
   }
 
 }
